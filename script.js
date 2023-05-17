67b1a9c646745f799c7258c3050b1b70de243163
Negrea-Andrei <negrea97@yahoo.com>
2023-05-17 20:14:39 +0300

    Added a function in js that creates new books and pushes them to an array

--- script.js
+++ script.js
@@ -3,33 +3,38 @@
 const form = document.querySelector(".display-form");
 const formPage = document.querySelector(".form-container");
 
+const submit = document.querySelector(".submit")
 //Array where the created books are stored
 const books = [];
 
 //book constructor
 function book(title, author, pages, read) {
-    this.title = title,
-        this.author = author,
-        this.pages = pages,
-        this.read = read
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 //function that creates the books and add them to the array
 function addBooks() {
-    const title = document.getElementById("#book-title").value;
-    const author = document.getElementById("#author-name").value;
-    const pages = document.getElementById("#pages-number").value;
-    const read = document.getElementById("#read").ariaChecked;
-    
+    const title = document.getElementById("book-title").value;
+    const author = document.getElementById("author-name").value;
+    const pages = document.getElementById("pages-number").value;
+    const read = document.getElementById("read").ariaChecked;
+
     //create the book
     const newBook = new book(title, author, pages, read);
 
     //add the new book to the array
     books.push(newBook);
+
+    console.log(books)
 }
 
-
-
+submit.addEventListener("click", event => {
+    event.preventDefault()
+    addBooks()
+});
 
 //Make the form appear
 let visible = false;
