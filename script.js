419eac9256dd9869da16f3b965fa84d9e3365f03
Negrea-Andrei <negrea97@yahoo.com>
2023-05-18 19:58:01 +0300

    Added a function that displays the books on the grid

--- script.js
+++ script.js
@@ -3,7 +3,12 @@
 const form = document.querySelector(".display-form");
 const formPage = document.querySelector(".form-container");
 
+//Get the submit button
 const submit = document.querySelector(".submit")
+
+//Link the grid to a variable
+const grid = document.querySelector(".right")
+
 //Array where the created books are stored
 const books = [];
 
@@ -15,7 +20,59 @@ function book(title, author, pages, read) {
     this.read = read;
 }
 
-//function that creates the books and add them to the array
+//book display function
+function displayBooks() {
+    books.forEach(element => {
+        grid.innerHTML = ''
+
+        //get the index of the book for the delete button
+        let index = books.indexOf(element);
+
+        //prepare the book card with all the information about the book
+        let bookCard = document.createElement('div');
+        let bookTitle = document.createElement('div');
+        let bookAuthor = document.createElement('div');
+        let bookPages = document.createElement('div');
+        let bookRead = document.createElement('input');
+
+        //Delete button
+        let bookDelete = document.createElement('button');
+        bookDelete.innerHTML = "Delete"
+
+        bookTitle.innerHTML = element.title;
+        bookAuthor.innerHTML = `Author: ${element.author}`;
+        bookPages.innerHTML = `Pages: ${element.pages}`;
+
+        // Set the checked property of bookRead based on the value of read checkbox
+        bookRead.type = 'checkbox';
+        bookRead.checked = element.read;
+
+        //Append the parts of the book to the book card
+        bookCard.appendChild(bookTitle).className = "title-card";
+        bookCard.appendChild(bookAuthor).className = "author-card";
+        bookCard.appendChild(bookPages).className = "pages-card";
+        bookCard.appendChild(bookRead).className = "checked-card";
+        bookCard.appendChild(bookDelete).className = "delete";
+
+        //Append the book-card to the grid
+        grid.appendChild(bookCard).className = "book-card";
+
+        //Delete function
+        function deleteBook() {
+            books.splice(index, 1)
+        }
+
+        function test() {
+            deleteBook();
+            displayBooks();
+        }
+
+        const deleteButton = document.querySelector(".delete");
+        deleteButton.addEventListener('click', test)
+    })
+}
+
+//function that add them to the array
 function addBooks() {
     const title = document.getElementById("book-title").value;
     const author = document.getElementById("author-name").value;
@@ -27,12 +84,12 @@ function addBooks() {
 
     //add the new book to the array if the inputs are correct
     if (!title || !author || !pages) {
-       alert("Please fill all the inputs")
+        alert("Please fill all the inputs")
     }
     else {
-        books.push(newBook);
-        console.log(books)
-    }    
+        books.push(newBook);        
+        displayBooks();
+    }
 }
 
 submit.addEventListener("click", event => {
@@ -47,20 +104,13 @@ let visible = false;
 function visibility() {
     if (visible == false) {
         visible = true;
+        formPage.style.setProperty('display', 'block')
     }
     else if (visible == true) {
         visible = false;
-    }
-}
-
-function formVisible() {
-    visibility();
-    if (visible == true) {
-        formPage.style.setProperty('display', 'block')
-    }
-    else if (visible == false) {
         formPage.style.setProperty('display', 'none')
-    }
+    };
 }
 
-form.addEventListener('click', formVisible)
+
+form.addEventListener('click', visibility)
